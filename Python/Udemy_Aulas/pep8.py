6f8b66ff004367979dbf5a74f84a62f73900b0a2
kassio <kassio@msn.com>
2021-02-01 13:02:16 -0300

    Up

---
+++ Python/Udemy_Aulas/pep8.py
@@ -0,0 +1,12 @@
+"""
+PEP8 - Python enhancement Proposal
+
+São propostas de melhorias para a linguagem Python
+
+Zem of Python
+
+import this
+A Ideia da PEP8 é que possamos escrever códigos Python de forma Pythonica.
+
+[1] - Utilize Camel Case para nomes de Classes;
+"""
