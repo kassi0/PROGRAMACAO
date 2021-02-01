6e7222c25f254c0563965f57d04f797afe7a20df
kassio <kassio@msn.com>
2021-02-01 14:38:00 -0300

    up

--- Python/Udemy_Aulas/pep8.py
+++ Python/Udemy_Aulas/pep8.py
@@ -9,4 +9,36 @@ import this
 A Ideia da PEP8 é que possamos escrever códigos Python de forma Pythonica.
 
 [1] - Utilize Camel Case para nomes de Classes;
+
+[2] - Utilizar nomes em minusculo, separados por underline (_)
+ para funcões ou variáveis;
+
+[3] - Utilize 4 espaços para identação!!!
+ 
 """
+
+
+"[1] - Utilize Camel Case para nomes de Classes;"
+class Calculadora:
+    pass
+
+class CalculadoraCientifica:
+    pass
+
+"[2] - Utilizar nomes em minusculo, separados por underline (_) para funcões ou variáveis;"
+
+def soma():
+    pass
+
+def soma_dois():
+    pass
+
+numero = 4
+
+numero_impar = 5
+
+
+"[3] - Utilize 4 espaços para identação!!! (Não Use TAB)"
+
+if 'a' in 'banana':
+    print ('Tem')
