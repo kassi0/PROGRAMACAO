626065ee4dc38925a681d5657ed6151a42f7e470
kassi0 <kassio@msn.com>
2021-02-22 17:24:09 -0300

    up

---
+++ Python/Udemy_Aulas/Help - Metodos.py
@@ -0,0 +1,21 @@
+'''
+Utilitários Python para que auxiliar na programação.
+
+dir -> Apresenta todos os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+    dir(tipodo dado/variável)
+
+help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis para determinado tipo de dado ou variável.
+'''
+
+"TEXTO" .lower() # Retorna copia com todas as letras minusculas
+    #lower() method of builtins.str instance
+    #Return a copy of the string converted to lowercase.
+"teXTo" .upper() # Retorna copia com todas as letras Maiusculas
+    #upper() method of builtins.str instance
+    #Return a copy of the string converted to uppercase.
+"texto" .title() # Retorna copia com as primeiras letras maiusculas
+    #title() method of builtins.str instance
+    #Return a version of the string where each word is titlecased.
+    #More specifically, words start with uppercased characters and all remaining
+    #cased characters have lower case.
