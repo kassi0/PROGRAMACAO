4ae934aa32b80b92edf12db145f13214a697cab2
kassi0 <kassio@msn.com>
2021-02-22 21:15:23 -0300

    Pycharm

--- Python/Udemy_Aulas/recebendo_dados_usuarios.py
+++ Python/Udemy_Aulas/recebendo_dados_usuarios.py
@@ -1,14 +1,15 @@
 """
  Recebendo dados do Usuário
 """
-
-
-
 # Entrada de Dados
 print('Qual Seu nome? ')
-nome = input()
+nome = input() # Input -  Entrada
 
+print('Seja bem vindo %s' % nome)
+
+print('Qual sua idade? ')
+idade = input()
 # Processamento
 
 # Saida de Dados
-print('Seja bem vindo %s' % nome)
+print('%s tem %s anos' %(nome, idade))
