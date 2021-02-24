13358561fb4949d75871774497b369289497ef33
kassi0 <kassio@msn.com>
2021-02-24 19:23:13 -0300

    Python

--- Python/Udemy_Aulas/Seção_5/Nascimento.py
+++ Python/Udemy_Aulas/Seção_5/Nascimento.py
@@ -1,7 +1,7 @@
 from datetime import date
 import datetime
 
-#Entrada de valores
+# Entrada de valores
 print('='*18)
 print('Exercício 2: idade')
 print('='*18)
@@ -9,20 +9,11 @@ nome = input("Como se chama? ")
 ano = eval(input("Nasceu em que ano? "))
 mes = eval(input("Nasceu em que mês? "))
 dia = eval(input("Nasceu em que dia? "))
-# ano_atual = eval(input("Ano atual? "))
-# mes_atual = eval(input("Mês atual? "))
-# dia_atual = eval(input("Dia atual? "))
 dataNasc = datetime.date(ano, mes, dia)
 dataAtual = date.today()
 
- # diferença retorna em timedelta
+# diferença retorna em timedelta
 diferenca = (dataAtual - dataNasc)
- # Cálculos e Resultados
+# Cálculos e Resultados
 rslt = (diferenca.days / 365.25)
- # ano_atual-ano
 print("O %s tem %d anos!" %(nome, rslt))
-#if (dia == dia_atual and mes == mes_atual):
-  #  print("O %s tem %d anos!" %(nome, rslt))
-#else:
-  #  ((dia > dia_atual and mes == mes_atual) or mes < mes_atual)
-  #  print("O %s tem %d anos!" %(nome, rslt))
