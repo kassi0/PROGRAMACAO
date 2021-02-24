fee0e3899e0c956890d0dcbff5ca286b308ebf97
kassio <kassio@msn.com>
2021-02-24 10:33:05 -0300

    Python

---
+++ Python/Udemy_Aulas/Seção_5/condicionais_if_else_elfi.py
@@ -0,0 +1,15 @@
+"""
+Estruturas condicionais
+if (se), else, elfi
+"""
+
+"""
+Estrutura Condicional if, em C
+
+if(idade < 18 {
+    printf("menor de idade");
+}
+"""
+
+if idade < 18:
+    print('Menor de idade')
