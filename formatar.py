65c2c433d0f37fa85df358fc5938d6035fe864e9
kassi0 <kassio@msn.com>
2021-04-29 11:33:04 -0300

    update

--- formatar.py
+++ formatar.py
@@ -13,28 +13,39 @@ isExiste = os.path.exists(pathe)
 while (True):
     os.system('cls' if os.name == 'nt' else 'clear')
     if isExistb is True:
-
-        print('PenDrive conectado, sendo formatado!')
-        subprocess.call(["mkfs.vfat /dev/sdb -I"], shell=True)
-        print("Pendrive Formatado Aguardando o Proximo")
+        print('\nPenDrive conectado, Formatando /DEV/SDB!\n')
+        subprocess.call(['mkfs.vfat -I -v -n "JEC-FORMATADO" /dev/sdb'], shell=True)
+        print("\nPendrive /DEV/SDB Formatado indo para Próximo")
         time.sleep(5)
     if isExistc is True:
-
-        print('PenDrive conectado, sendo formatado!')
-        subprocess.call(["mkfs.vfat /dev/sdc -I"], shell=True)
-        print("Pendrive Formatado Aguardando o Proximo")
+        print('\nPenDrive conectado, Formatando /DEV/SDC!\n')
+        subprocess.call(['mkfs.vfat -I -v -n "JEC-FORMATADO" /dev/sdc'], shell=True)
+        print("\nPendrive /DEV/SDC Formatado indo para Próximo")
         time.sleep(5)
     if isExistd is True:
-
-        print('PenDrive conectado, sendo formatado!')
-        subprocess.call(["mkfs.vfat /dev/sdd -I"], shell=True)
-        print("Pendrive Formatado Aguardando o Proximo")
+        print('\nPenDrive conectado, Formatando /DEV/SDD!\n')
+        subprocess.call(['mkfs.vfat -I -v -n "JEC-FORMATADO" /dev/sdd'], shell=True)
+        print("\nPendrive /DEV/SDD Formatado indo para Próximo")
         time.sleep(5)
     if isExiste is True:
+        print('\nPenDrive conectado, Formatando /DEV/SDE!\n')
+        subprocess.call(['mkfs.vfat -I -v -n "JEC-FORMATADO" /dev/sde'], shell=True)
+        print("\nPendrive /DEV/SDE Formatado Aguardando próxima Remessa")
+        time.sleep(50)
 
-        print('PenDrive conectado, sendo formatado!')
-        subprocess.call(["mkfs.vfat /dev/sde -I"], shell=True)
-        print("Pendrive Formatado Aguardando o Proximo")
-        time.sleep(5)
+    if isExistb is False:
+        print('Não Encontrei algum dos PenDrives')
+        time.sleep(20)
 
+    elif isExistc is False:
+        print('Não Encontrei algum dos PenDrives')
+        time.sleep(20)
+
+    elif isExistd is False:
+        print('Não Encontrei algum dos PenDrives')
+        time.sleep(20)
+
+    elif isExiste is False:
+        print('Não Encontrei algum dos PenDrives')
+        time.sleep(20)
 
